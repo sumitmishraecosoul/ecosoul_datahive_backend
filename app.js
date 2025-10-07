0a57229f478f99b4a87773dfd37dadadfaf61a72
Harshit Kant <Harshit.Kant@ecosoulhome.com>
2025-10-07 13:54:02 +0530

    cors changed

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: 'https://worklytics.thrivebrands.ai',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true
