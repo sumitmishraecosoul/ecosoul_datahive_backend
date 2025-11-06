d641ad1744f017063ac5a0264093514bea3cdf6b
Harshit Kant <Harshit.Kant@ecosoulhome.com>
2025-11-06 16:58:49 +0530

    domain changed

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors(
     {
-        // origin: 'https://datahive.vectoraistudio.com',
-		origin: 'http://localhost:7600',	
+        origin: 'https://datahive.vectoraistudio.com',
+		// origin: 'http://localhost:7600',	
         methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true
